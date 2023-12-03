f8cd2267eaacbbae9ccfe700a9ee5d1421851d44
fruz@mac <fruz@192.168.1.5>
2023-12-03 12:37:57 +0700

    deleting unneneccesary routes

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function() {
-    return response('<h1>Hello there!</h1>', 200)
-    ->header('Content-Type', 'text/plain')
-    ->header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function($id){
-    dd($id);
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-
-
-Route::get('/search', function(Request $request){
-    return $request->name . ' ' . $request->city;
-});
