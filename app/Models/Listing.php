9c962922f681fcf16cd1c8e6c3b9763408c994ec
ScaferuZ <endrafruz@gmail.com>
2023-12-07 21:58:52 +0700

    creating listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,29 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Job 1',
-                'description' => 'This is a job description',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Job 2',
-                'description' => 'This is a job description',
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-        
-        foreach ($listings as $listing){
-            if ($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
